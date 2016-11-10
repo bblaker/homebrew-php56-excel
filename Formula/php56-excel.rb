4bcd7c4df216d1454dcebce13d232897a8bbde98
Ben Blaker <bblaker@propertysolutions.com>
2016-11-10 16:49:10 -0700

    adding checksum for php_excel requirement

--- Formula/php56-excel.rb
+++ Formula/php56-excel.rb
@@ -5,6 +5,7 @@ class Php56Excel < AbstractPhp56Extension
   homepage 'https://github.com/iliaal/php_excel'
   url 'https://github.com/iliaal/php_excel/releases/download/Excel-1.0.2-PHP5/excel-1.0.2-php5.tgz'
   head 'https://github.com/iliaal/php_excel.git'
+  sha256 '3620527fd821899747f52cfe4bb9e8243c00a5f5ca9c55c5fb320424d2be4673'
 
   depends_on 'autoconf' => :build
   depends_on 'php56' unless build.include?('without-homebrew-php')
